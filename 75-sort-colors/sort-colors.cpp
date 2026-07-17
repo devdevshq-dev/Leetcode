8f515c031d3d247406a9106125e0bba32bda3336
devdevshq-dev <devdev.shq@gmail.com>
2026-07-18 01:14:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.75%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        /*
+            1. define 3 vars low, mid and high as 0,0 and n - 1
+            2. run loop like binary search from mid <= high
+            3. if mid == 0 swap(arr[low++], arr[mid++])
+            4. if mid == 1 mid++
+            5. if mid == 2 swap(arr[high--], arr[mid])
+        */
         int low = 0, mid = 0, high = nums.size() - 1;
         while (mid <= high) {
             if (nums[mid] == 2) {
-                swap(nums[mid], nums[high]);
-                high--;
+                swap(nums[mid], nums[high--]);
             } else if (nums[mid] == 0) {
-                swap(nums[mid], nums[low]);
-                mid++;
-                low++;
+                swap(nums[mid++], nums[low++]);
             } else {
                 mid++;
             }
