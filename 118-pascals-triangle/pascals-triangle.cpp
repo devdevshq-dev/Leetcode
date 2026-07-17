1fe6bfcd8f30340e62ac4c8d17194b37c53d7f28
devdevshq-dev <devdev.shq@gmail.com>
2026-07-18 00:26:50 +0530

    Time: 3 ms (7.86%) | Memory: 9.7 MB (63.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> sol;
-        sol.push_back({1});
-        if (numRows == 1) return sol;  
-        sol.push_back({1,1}); 
-        if (numRows == 2) return sol;
-        numRows -= 2;
-        while (numRows) {
-            vector<int> temp(sol.back().size() + 1);
-            temp[0] = 1;
-            temp[sol.back().size()] = 1;
-            for (int i = 1; i < sol.back().size(); i++) {
-                temp[i] = sol.back()[i-1] + sol.back()[i];
+        int curRow = 1;
+        int val = 1;
+        while (numRows--) {
+            vector<int> temp;
+            temp.push_back(1);
+            for (int i = 1; i < curRow; i++) {
+                val = val * (curRow - i) / i;
+                temp.push_back(val);
             }
+            curRow++;
             sol.push_back(temp);
-            numRows--;
         }
         return sol;
     }
