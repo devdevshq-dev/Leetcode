0d0b1335ad4fabde3e5e53bf5ce1b46589068c94
devdevshq-dev <devdev.shq@gmail.com>
2026-07-25 01:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.07%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    double power(double x, long long n) {
+        if (n == 0)
+            return 1.0;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+
+        return x * half * half;
+    }
     double myPow(double x, int n) {
         long long exp = n;
 
@@ -8,16 +19,6 @@ public:
             exp = -exp;
         }
 
-        double ans = 1.0;
-
-        while (exp > 0) {
-            if (exp & 1)
-                ans *= x;
-
-            x *= x;
-            exp >>= 1;
-        }
-
-        return ans;
+        return power(x, exp);
     }
 };
